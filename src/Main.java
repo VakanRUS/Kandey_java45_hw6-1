d366c83cefccd7a051c3b0f2e41d0e44bce2716a
Vakan <vakan1@yandex.ru>
2025-05-02 14:34:50 +0300

    done homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
+        //task 1
+        System.out.println("task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = (lastName + " " + firstName + " " + middleName);
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+        System.out.println();
+
+        //task 2
+        System.out.println("task 2");
+        String upperFullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + upperFullName);
+        System.out.println();
+
+        //task 3
+        System.out.println("task 3");
+        fullName = "Иванов Семён Семёнович";
+        String correctedFullName = fullName.replace("ё", "е");
+        System.out.println("Данные Ф. И. О. сотрудника — " + correctedFullName);
+        System.out.println();
     }
 }
